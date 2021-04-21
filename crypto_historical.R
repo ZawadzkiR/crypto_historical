cdacef04aba75ecb26c1b366044e8551b974b7de
rz384365 <72023108+rz384365@users.noreply.github.com>
2021-04-21 23:26:31 +0200

    Add files via upload

---
+++ crypto_historical.R
@@ -0,0 +1,17 @@
+getprice = function(symbol='ethereum', start_date='2011-01-01', end_date='2021-04-21'){
+  library('rvest')
+  webpage = read_html(paste0("https://www.coingecko.com/en/coins/",symbol,
+                             "/historical_data/usd?start_date=", start_date,"&end_date=",end_date,"#panel"))
+  tbls = html_nodes(webpage, "tr")
+  table = tbls %>% html_text()
+  rows = gsub(',,',',',gsub(' ','',gsub('.{1}$','\n',gsub('\n',',',gsub('\n\n',',',gsub('\n\n\n',',',
+                                                                                        gsub('\\$','',gsub(',', '' ,table))))))))
+  to_df = paste(rows, collapse = '')
+  df = read.csv(text=to_df)
+  df = df[-1,]
+  df$Close = as.numeric(df$Close)
+  
+  return (df)
+  
+  
+}
